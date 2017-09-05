a40b060d58b4f6f5fabd8c7df933b82fa57f53c3
xiejun <592942092@qq.com>
2017-09-05 18:01:12 +0800

    a

--- SpringMVCHibernateManyToManyCRUDExample/src/main/java/com/xj/springmvc/security/SecurityWebApplicationInitializer.java
+++ SpringMVCHibernateManyToManyCRUDExample/src/main/java/com/xj/springmvc/security/SecurityWebApplicationInitializer.java
@@ -1,7 +1,7 @@
 package com.xj.springmvc.security;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
+//注册过滤器 springSecurityFilterChain
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
