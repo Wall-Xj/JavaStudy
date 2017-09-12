e115a297373adb6e26325e2a2ffff2851a9c9451
xiejun <592942092@qq.com>
2017-09-12 18:06:40 +0800

    a

--- ssh_secutrity2/src/main/java/com/xj/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.xj.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//注册过滤器 springSecurityFilterChain
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
